3648666737d4793cfd99672285e814e6e516adc8
iury souza <iurysza@users.noreply.github.com>
2026-09-14 18:42:47 +0000

    chore: exclude .agents from vitest

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,6 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     restoreMocks: true,
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/cypress/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-      '**/.agents/**',
-    ],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.agents/**'],
   },
 })
